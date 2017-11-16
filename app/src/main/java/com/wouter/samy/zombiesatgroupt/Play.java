315600b5b8176ae2c336716bd2e75b5d1ed66e39
samyv <samyvanv@hotmail.com>
2017-11-16 19:58:26 +0100

    Username and password in playActivity

--- app/src/main/java/com/wouter/samy/zombiesatgroupt/Play.java
+++ app/src/main/java/com/wouter/samy/zombiesatgroupt/Play.java
@@ -2,6 +2,8 @@ package com.wouter.samy.zombiesatgroupt;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Play extends AppCompatActivity {
 
@@ -9,5 +11,19 @@ public class Play extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
+    }
+
+    public void loginButton(View view){
+        TextView usernameTv = findViewById(R.id.username);
+        String username = usernameTv.getText().toString();
+
+        TextView passwordTv = findViewById(R.id.password);
+        String password = passwordTv.getText().toString();
+
+
+    }
+
+    public void registerButton(View view){
+
     }
 }
