567886d95371aeeca260bb293a3910867418cbe4
wouterheerwegh <r0632310@kuleuven.be>
2017-11-17 11:10:05 +0100

    fade

--- app/src/main/java/com/wouter/samy/zombiesatgroupt/MainActivity.java
+++ app/src/main/java/com/wouter/samy/zombiesatgroupt/MainActivity.java
@@ -46,6 +46,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void exitButton(View view) {
-
+        startActivity(new Intent(this, test.class));
     }
 }
---
+++ app/src/main/java/com/wouter/samy/zombiesatgroupt/test.java
@@ -0,0 +1,90 @@
+package com.wouter.samy.zombiesatgroupt;
+
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.graphics.drawable.Drawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.ImageView;
+
+public class test extends AppCompatActivity {
+    ImageView image;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+        image = findViewById(R.id.imageView);
+
+    }
+    public void nextpic(View view){
+        ObjectAnimator fadeOut = ObjectAnimator.ofFloat(image, View.ALPHA, 1,0);
+        ObjectAnimator fadeIn  = ObjectAnimator.ofFloat(image, View.ALPHA, 0,1);
+        image.clearAnimation();
+        AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.play(fadeOut);
+        animatorSet.start();
+        String uri = "@drawable/ballon";
+        int imageResource = getResources().getIdentifier(uri, null, getPackageName());
+        Drawable res = getResources().getDrawable(imageResource);
+        image.setImageDrawable(res);
+        animatorSet.play(fadeIn);
+        animatorSet.start();
+    }
+
+
+    private void animate(final ImageView imageView, final int images[], final int imageIndex, final boolean forever) {
+
+        //imageView <-- The View which displays the images
+        //images[] <-- Holds R references to the images to display
+        //imageIndex <-- index of the first image to show in images[]
+        //forever <-- If equals true then after the last image it starts all over again with the first image resulting in an infinite loop. You have been warned.
+
+        int fadeInDuration = 500; // Configure time values here
+        int timeBetween = 3000;
+        int fadeOutDuration = 1000;
+
+        imageView.setVisibility(View.INVISIBLE);    //Visible or invisible by default - this will apply when the animation ends
+        imageView.setImageResource(images[imageIndex]);
+
+        Animation fadeIn = new AlphaAnimation(0, 1);
+        fadeIn.setInterpolator(new DecelerateInterpolator()); // add this
+        fadeIn.setDuration(fadeInDuration);
+
+        Animation fadeOut = new AlphaAnimation(1, 0);
+        fadeOut.setInterpolator(new AccelerateInterpolator()); // and this
+        fadeOut.setStartOffset(fadeInDuration + timeBetween);
+        fadeOut.setDuration(fadeOutDuration);
+
+        AnimationSet animation = new AnimationSet(false); // change to false
+        animation.addAnimation(fadeIn);
+        animation.addAnimation(fadeOut);
+        animation.setRepeatCount(1);
+        imageView.setAnimation(animation);
+
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            public void onAnimationEnd(Animation animation) {
+                if (images.length - 1 > imageIndex) {
+                    animate(imageView, images, imageIndex + 1, forever); //Calls itself until it gets to the end of the array
+                } else {
+                    if (forever == true) {
+                        animate(imageView, images, 0, forever);  //Calls itself to start the animation all over again in a loop if forever = true
+                    }
+                }
+            }
+
+            public void onAnimationRepeat(Animation animation) {
+                // TODO Auto-generated method stub
+            }
+
+            public void onAnimationStart(Animation animation) {
+                // TODO Auto-generated method stub
+            }
+        });
+    }
+}
